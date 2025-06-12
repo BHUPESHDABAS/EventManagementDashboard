41df8f517c8dd598c06ee226f389b23f1cad7103
“Bhupesh <dabasbhupesh78@gmail.com>
2025-06-12 15:00:59 +0530

    switching to mongoDB atlas

--- backend/controllers/events.js
+++ backend/controllers/events.js
@@ -1,13 +1,18 @@
 import Event from "../models/Event.js"
 
-export const createEvent = async () => {
-    const {heading, imgUrl, description } = req.body;
-    const newEvent = new Event({
-        eventHeading: heading,
-        eventDescription: description,
-        eventImageUrl: imgUrl
-    })
+export const createEvent = async (req,res) => {
+    try {
+        const {heading, imgUrl, description } = req.body;
+        const newEvent = new Event({
+            eventHeading: heading,
+            eventDescription: description,
+            eventImageUrl: imgUrl
+        })
 
     await newEvent.save();
     res.status(201).json({ newEvent, message: "Event added successfully" });
+    } catch (error) {
+        console.log(error)
+        res.status(201).json({message: "Event was not added, try again" });
+    }
 }
--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 import express from "express";
 import mongoose from "mongoose";
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
@@ -14,14 +14,29 @@ app.use("/auth", authRoute);
 
 import eventRoute from "./routes/events.js";
 app.use("/user", eventRoute);
+app.use("/admin", eventRoute);
 
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+    app.listen(PORT, () =>
+      console.log(`http://localhost:${PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.error("connection error:", err);
+  });
 
-mongoose.connect(`${process.env.DB_PATH}/${process.env.DB_NAME}`)
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log("http://localhost:" + PORT);
-        })
-    })
-    .catch(err => {
-        console.log(err);
-    })
+// mongoose.connect(`${process.env.DB_PATH}/${process.env.DB_NAME}`)
+//     .then(() => {
+//         app.listen(PORT, () => {
+//             console.log("http://localhost:" + PORT);
+//         })
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
