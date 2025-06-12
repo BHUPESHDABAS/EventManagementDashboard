4a65ed455c78a97885423c553a3f67b3e591f317
“Bhupesh <dabasbhupesh78@gmail.com>
2025-06-12 15:37:56 +0530

    Adding Admin CRUD functions

--- backend/controllers/events.js
+++ backend/controllers/events.js
@@ -1,5 +1,6 @@
 import Event from "../models/Event.js"
 
+//Admin functions
 export const createEvent = async (req,res) => {
     try {
         const {heading, imgUrl, description } = req.body;
@@ -9,10 +10,41 @@ export const createEvent = async (req,res) => {
             eventImageUrl: imgUrl
         })
 
-    await newEvent.save();
-    res.status(201).json({ newEvent, message: "Event added successfully" });
+        await newEvent.save();
+        res.status(201).json({ newEvent, message: "Event added successfully" });
+    } catch (error) {
+        // console.log(error)
+        res.status(201).json({message: "Event was not added, try again" });
+    }
+}
+
+export const updateEvent = async (req,res) => {
+    try {
+        const {heading, imgUrl, description, eventId} = req.body;
+        const findEvent = await Event.findOne({_id: eventId});
+
+        findEvent.eventDescription = description;
+        findEvent.eventHeading = heading;
+        findEvent.eventImageUrl = imgUrl;
+
+        await findEvent.save();
+        res.status(201).json({ findEvent, message: "Event updated successfully" });
+
+    } catch (error) {
+        // console.log(error)
+        res.status(201).json({message: "Unable to update, try again" });
+    }
+}
+
+export const deleteEvent = async (req,res) => {
+    try {
+        const {eventId} = req.body;
+        const findEvent = await Event.deleteOne({_id: eventId});
+
+        res.status(201).json({ findEvent, message: "Event deleted successfully" });
+
     } catch (error) {
         console.log(error)
-        res.status(201).json({message: "Event was not added, try again" });
+        res.status(201).json({message: "Unable to delete, try again" });
     }
 }
--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {createEvent} from "../controllers/events.js"
+import {createEvent, updateEvent, deleteEvent} from "../controllers/events.js"
 const router = express.Router();
 
-
+//Admin Routes
 router.post("/createEvent", createEvent);
+router.post("/updateEvent", updateEvent);
+router.post("/deleteEvent", deleteEvent);
 
 
 export default router;
