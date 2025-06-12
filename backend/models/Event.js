e1fb1803cb5e412640ddec3d58c97b71161ab658
“Bhupesh <dabasbhupesh78@gmail.com>
2025-06-12 12:58:39 +0530

    adding Events model

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -2,19 +2,30 @@ import mongoose from "mongoose";
 import User from "../models/User.js";
 
 const eventSchema = new mongoose.Schema({
-    eventHeading: {
-        type: String,
-        required: true
-    },
-    eventImageUrl: {
-        type: String,
-        required: true
-    },
-    eventOrganiser: {
-        type: String,
-        required: true
+  eventHeading: {
+    type: String,
+    required: true,
+  },
+  eventImageUrl: {
+    type: String,
+    required: true,
+  },
+  eventOrganiser: {
+    type: String,
+    ref: "User",
+    required: true,
+  },
+});
+
+eventSchema.pre("save", async function (next) {
+  if (!this.eventOrganiser) {
+    const admin = await User.findOne({ email: "infiniteLocus@gmail.com" });
+    if (!admin) {
+      return next(new Error("Admin user not found"));
     }
-    
-})
+    this.eventOrganiser = admin._id;
+  }
+  next();
+});
 
 export default mongoose.model("Events", eventSchema);
